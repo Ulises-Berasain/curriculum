a2f09466af4494728c09f3c49d9edbebc3b1662b
Zarek-Reso <uli_cuervo_79@hotmail.com>
2021-05-07 21:11:06 -0300

    Actualizacion del componente About Me
    corrección de errores ortográficos y mejora en el texto.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,11 +10,11 @@ export default function AboutMe(){
                 <img className="profile-picture"src={profilePicture}/>
             </div>
             <div className="container-info">
-                <p>Mi nombre es <strong>Ulises Berasain</strong>, naci el <strong>11 de Marzo de 1994</strong> y actualmente resido en <strong>Azul, Provincia de Buenos Aires, Argentina</strong>.<br/>
+                <p>Mi nombre es <strong>Ulises Berasain</strong>, naci el <strong>11 de Marzo de 1994</strong> y resido en <strong>Azul, Provincia de Buenos Aires, Argentina</strong>.<br/>
                     Actualmente me encuentro estudiando de forma autodidacta Desarrollo Web para poder convertirme en <strong>Full Stack Web Developer</strong>.<br/>
                     Por el momento me desempeño como <strong>Front-End Web Developer Jr.</strong>, sigo siempre estudiando y empezando proyectos personales nuevos.<br/>  
-                    Busco poder seguir creciendo en lo que es la industria IT para poder estar en constante desarrollo tanto como persona y profecionalmente.<br/> 
-                    Siempre estoy estudiando y buscando aprender algo nuevo, estar al tanto de las nuevas tecnologias que van saliendo y si son necesarias poder aprenderlas y dominarlas.
+                    Busco poder seguir creciendo en lo que es la industria IT para poder estar en constante desarrollo tanto como persona y profesionalmente.<br/> 
+                    Siempre estoy estudiando y buscando aprender algo nuevo, estar al tanto de las nuevas tecnologías que van saliendo y si son necesarias poder aprenderlas y dominarlas.
                 </p>
             </div>
         </div>
